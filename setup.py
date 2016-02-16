af1aff782339052a8bf1f26294504b845736cf5d
Oskari Rauta <osku@iMac.local>
2016-02-16 15:22:26 +0200

    Update setup tools script

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 PACKAGE = 'periscope'
 
 setup(name = PACKAGE,
-      version = 'dev',
+      version = 'subsearch',
       license = "GNU LGPL",
-      description = "Python module to download subtitles for a given video file",
-      author = "Patrick Dessalle",
-      author_email = "patrick.dessalle@gmail.com",
+      description = "Fork of periscope used for downloading subtitles for given video file(s) from SubHeaven.org or SubFinland.org through Subsearch.org's search engine",
+      author = "Oskari Rauta",
+      author_email = "oskari.rauta@gmail.com",
       url = "http://code.google.com/p/periscope/",
       packages= [ "periscope", "periscope/plugins" ],
       py_modules=["periscope"],
-      scripts = [ "bin/periscope" ],
-      install_requires = ["BeautifulSoup >= 3.2.0"]
+      install_requires = ["beautifulsoup4"]
       )
